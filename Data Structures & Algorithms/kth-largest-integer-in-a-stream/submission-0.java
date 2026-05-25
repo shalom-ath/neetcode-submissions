83edb8b86053d79b0d2d38b6d901b1cd6bcb7379
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-05-25 23:40:08 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,28 @@
+class KthLargest {
+
+    private final PriorityQueue<Integer> minHeap;
+    private final int k;
+
+    public KthLargest(int k, int[] nums) {
+
+        this.k = k;
+        minHeap = new PriorityQueue();
+
+        for(int el : nums){
+
+            add(el);
+        }    
+    }
+    
+    public int add(int val) {
+        
+            minHeap.add(val);
+
+            if(minHeap.size() > k){
+
+                minHeap.poll();
+            }
+
+        return minHeap.peek();    
+    }
+}
