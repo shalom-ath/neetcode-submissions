8c461bb928f499863accce16bf8d502a72e89e82
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-05-26 22:49:19 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        for(char task : tasks){
+
+            freqMap.put(task, freqMap.getOrDefault(task,0) + 1);
+        }
+
+        //Queue - 
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b - a);
+
+        maxHeap.addAll(freqMap.values());
+
+        //processing intervals
+
+        int time = 0;
+
+        while(!maxHeap.isEmpty()){
+
+            List<Integer> temp = new ArrayList<>();
+
+            for(int i = 0 ;i < n + 1; i++){
+
+                if(!maxHeap.isEmpty()){
+
+                    temp.add(maxHeap.poll());
+                }
+            }
+
+            for(int freq : temp){
+
+                if(--freq > 0){
+
+                    maxHeap.add(freq);
+                }
+            }
+
+            time += maxHeap.isEmpty() ? temp.size() : n + 1;
+        }
+
+        return time;
+        
+    }
+}
