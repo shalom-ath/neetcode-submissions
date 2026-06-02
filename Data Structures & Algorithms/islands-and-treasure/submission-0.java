30f204867b6275c34d079a0a7d749c133d5144f7
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-03 02:30:37 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.java
@@ -0,0 +1,52 @@
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+
+        //edge case
+
+        if(grid == null || grid.length == 0 || grid[0].length == 0){
+
+            return;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int INF = Integer.MAX_VALUE;
+        int[] DIRS = {0, 1, 0, -1, 0};
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for(int i = 0; i < grid.length; i++){
+
+            for(int j = 0; j < grid[i].length; j++){
+
+                if(grid[i][j] == 0){
+
+                    queue.offer(new int[]{i,j});
+                }
+            }
+        }
+
+        //bfs
+
+        while(!queue.isEmpty()){
+
+            int[] currentgate = queue.poll();
+            int currentrow = currentgate[0];
+            int currentcol = currentgate[1];
+
+            for(int i = 0; i < 4; i++){
+
+                int neighrow = currentrow + DIRS[i];
+                int neighcol = currentcol + DIRS[i + 1];
+
+                if(neighrow >= 0 && neighrow < m && neighcol >= 0 && neighcol < n && grid[neighrow][neighcol] == INF){
+
+                    grid[neighrow][neighcol] = grid[currentrow][currentcol] + 1;
+
+                    queue.offer(new int[]{neighrow, neighcol});
+                }
+            }
+        }
+        
+    }
+}
