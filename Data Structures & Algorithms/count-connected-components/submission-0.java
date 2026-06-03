2c746e112203b94bc90b848f92e30e6bca400e06
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-04 03:36:37 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+
+        int[] parent = new int[n];
+
+        //own nodes to itself
+
+        for(int i = 0; i < parent.length; i++) parent[i] = i;
+
+        for(int[] edge : edges) union(edge[0], edge[1], parent);
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < parent.length; i++) set.add(find(i, parent));
+
+        return set.size();
+    }
+
+    private void union(int edge1, int edge2, int[] parent){
+
+       int parent1 = find(edge1, parent);
+        int parent2 = find(edge2, parent);
+
+        parent[parent1] = parent2;
+    }
+
+    private int find(int edge, int[] parent){
+
+        if(parent[edge] != edge) 
+
+        parent[edge] = find(parent[edge], parent);
+
+        return parent[edge];
+    }
+}
