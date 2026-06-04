60c2db2f720087d2f00bfd21385e6504ef05053d
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-04 23:45:19 +0530

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+
+        //edge case
+
+        if(edges.length != n -1)
+
+        return false;
+
+        List<List<Integer>> adjlist = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+
+            adjlist.add(new  ArrayList<>());
+
+        }
+
+        for(int[] edge : edges){
+
+            adjlist.get(edge[0]).add(edge[1]);
+            adjlist.get(edge[1]).add(edge[0]);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        Set<Integer> visited = new HashSet<>();
+
+        stack.push(0);
+        
+        visited.add(0);
+
+        while(!stack.isEmpty()){
+
+            int node = stack.pop();
+
+            for(int neighbors : adjlist.get(node)){
+
+                if(visited.contains(neighbors))
+
+                continue;
+
+                visited.add(neighbors);
+                stack.push(neighbors);
+
+            }
+
+        }
+
+         if(visited.size() == n){
+
+            return true;
+            }
+
+            return false;
+        
+
+
+
+    }
+}
