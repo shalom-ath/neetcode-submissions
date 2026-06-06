c05a0230e67c16f9071ba33b45e85efe8a93056c
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-06 23:28:12 +0530

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.java
@@ -0,0 +1,78 @@
+class Solution {
+
+    private class Node{
+
+        private int target;
+        private int distance;
+
+        public Node(int target, int distance){
+
+            this.target = target;
+            this.distance = distance;
+        }
+    }
+
+    public int networkDelayTime(int[][] times, int n, int k) {
+
+        //pq
+
+        PriorityQueue<Node> pq = new PriorityQueue<Node>((a,b)-> a.distance - b.distance);
+
+        Map<Integer, Set<Node>> map = buildgraph(times);
+
+        Set<Integer> visited = new HashSet<>();
+
+        pq.offer(new Node(k, 0));
+
+        int totaltime = 0;
+
+        while(!pq.isEmpty()){
+
+            int size = pq.size();
+
+            while(size -- > 0){
+
+                Node head = pq.poll();
+
+                if(visited.contains(head.target))
+                continue;
+
+                visited.add(head.target);
+
+                totaltime = Math.max(totaltime, head.distance);
+
+                if(visited.size() == n)
+
+                return totaltime;
+
+                if(map.containsKey(head.target)){
+
+                    for(Node children : map.get(head.target)){
+
+                        int childtime = children.distance + head.distance;
+
+                        pq.offer(new Node(children.target, childtime));
+                    }
+                }
+            }
+        }
+
+        return -1;
+        
+    }
+
+    private Map<Integer, Set<Node>> buildgraph(int[][] times){
+
+        Map<Integer, Set<Node>> map = new HashMap<>();
+
+        for(int[] time : times){
+
+            map.putIfAbsent(time[0], new HashSet<>());
+
+            map.get(time[0]).add(new Node(time[1], time[2]));
+
+        }
+
+        return map;
+    }
+}
