1ad89252abd37c267c19f598dc669fe663b8505b
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-08 03:07:40 +0530

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+        Set<String> words = new HashSet<>(wordList);
+
+        if(!words.contains(endWord))
+
+        return 0;
+
+        Queue<String> queue = new LinkedList<>();
+
+        queue.offer(beginWord);
+
+        Set<String> visited = new HashSet<>();
+
+        visited.add(beginWord);
+
+        int level = 1;
+
+        while(!queue.isEmpty()){
+
+            int size = queue.size();
+
+            while(size -- > 0){
+
+                String word = queue.poll();
+
+                if(word.equals(endWord)) return level;
+
+                for(int i = 0; i < word.length(); i++){
+
+                    char[] chars = word.toCharArray();
+
+                    for(char c = 'a'; c <= 'z' ; c++){
+
+                        if(chars[i] == c)
+
+                        continue;
+
+                        chars[i] = c;
+
+                        String nextword = new String(chars);
+
+                        if(words.contains(nextword)){
+
+                            queue.offer(nextword);
+
+                            words.remove(nextword);
+                        }
+                    }
+
+
+
+                } 
+            }
+
+            level++;
+        }
+
+        return 0;
+
+    }
+}
