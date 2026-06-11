145119e9524a0472503dca999ec3734e46524b05
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-12 01:01:38 +0530

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+
+        int[] dp = new int[nums.length];
+
+        for(int i = 1; i < nums.length; i++){
+
+            for(int j = 0; j < i; j++){
+
+                if(nums[i] > nums[j])
+
+                if(dp[j] + 1 > dp[i])
+
+                dp[i] = dp[j] + 1;
+            }
+        }
+
+        int maxindex = 0;
+
+        for(int i = 0; i < dp.length; i++){
+
+            if(dp[i] > dp[maxindex])
+
+            maxindex = i;
+        }
+
+        return dp[maxindex] + 1;
+        
+    }
+}
