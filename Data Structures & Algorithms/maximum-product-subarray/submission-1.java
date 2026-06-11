598ff3385fe44e0757ee1243e23e0e05159e1313
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-11 23:26:15 +0530

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        int n = nums.length;
+
+        int leftproduct = 1;
+        int rightproduct = 1;
+
+        int ans = nums[0];
+
+        for(int i = 0; i < n; i++){
+
+            //if 0
+
+            leftproduct = leftproduct == 0 ? 1 : leftproduct;
+
+            rightproduct = rightproduct == 0 ? 1 : rightproduct;
+
+            leftproduct *= nums[i];
+
+            rightproduct *= nums[n - 1 - i];
+
+            ans = Math.max(ans, Math.max(leftproduct, rightproduct));
+        }
+
+        return ans;
+        
+    }
+}
