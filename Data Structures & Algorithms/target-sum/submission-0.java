c7bb27b166570502bdb43f36933ce577e261d811
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-14 02:57:38 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+
+        Map<Integer, Integer> dp = new HashMap<>();
+
+        dp.put(0 , 1);
+
+        for(int num : nums){
+
+            Map<Integer, Integer> nextdp = new HashMap<>();
+
+            for(int sum : dp.keySet()){
+
+                int count = dp.get(sum);
+
+                nextdp.put(sum + num, nextdp.getOrDefault(sum + num, 0) + count);
+
+                nextdp.put(sum - num, nextdp.getOrDefault(sum - num, 0) + count);
+            }
+
+            dp = nextdp;
+        }
+
+        return dp.getOrDefault(target , 0);
+        
+    }
+}
