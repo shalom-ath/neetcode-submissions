8411df8c2b04a075c976dfa19ec072e162f32d9e
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-19 23:30:03 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int trap(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int ans = 0;
+        int leftmax = 0, rightmax = 0;
+
+        while(left < right){
+
+         leftmax = Math.max(leftmax, height[left]);
+         rightmax = Math.max(rightmax, height[right]);
+
+         if(leftmax < rightmax){
+
+            ans += leftmax - height[left];
+            
+            left++;
+         }
+
+         else{
+
+            ans += rightmax - height[right];
+            right--;
+         }
+        }
+
+        return ans;
+        
+    }
+}
