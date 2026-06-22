e993c861ea50895291309ab6d7954134d832aae3
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-23 02:30:48 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for(char ch : t.toCharArray()) mapT[ch]++;
+
+        int left = 0, right = 0, minlength = Integer.MAX_VALUE, minstart = 0;
+
+        for(; right < s.length(); right++){
+
+            mapS[s.charAt(right)]++;
+
+            while(contains(mapS, mapT)){
+
+                if(right - left + 1 < minlength){
+
+                    minlength = right - left + 1;
+                    minstart = left;
+                }
+
+                mapS[s.charAt(left++)]--;
+            }
+        }
+
+        return minlength == Integer.MAX_VALUE ? "" : s.substring(minstart , minstart + minlength);
+
+    }
+
+    private boolean contains(int[] mapS, int[] mapT){
+
+        for(int i = 0; i < 256; i++){
+
+            if(mapS[i] < mapT[i]){
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+        
+}
