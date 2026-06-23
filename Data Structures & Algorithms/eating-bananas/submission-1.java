8469c1f255626cbfd3855d924f06ea57a995605a
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-24 02:13:58 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int minspeed = 1;
+        int maxspeed = 0;
+
+        for(int pile : piles){
+
+            maxspeed = Math.max(maxspeed, pile);
+        }
+
+        while(minspeed < maxspeed){
+
+            int mid = minspeed + (maxspeed - minspeed) / 2;
+
+            if(canEatintime(piles, h, mid)){
+
+                maxspeed = mid;
+            }
+
+            else{
+
+                minspeed = mid + 1;
+            }
+        }
+
+        return minspeed;
+        
+    }
+
+    private boolean canEatintime(int[] piles, int h, int speed){
+
+        int hours = 0;
+
+        for(int pile : piles){
+
+            hours += (int) Math.ceil((double) pile / speed);
+        }
+
+        return hours <= h;
+    }
+}
