7c507338b63c79a40e3a185e5b3098271f650114
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-26 21:35:05 +0530

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean checkValidString(String s) {
+
+        int minopen = 0;
+        int maxopen = 0;
+
+        for(char ch : s.toCharArray()){
+
+            if(ch == '('){
+
+                minopen++;
+                maxopen++;
+            }
+
+            else if(ch == ')'){
+
+                minopen--;
+                maxopen--;
+            }
+
+            else{
+
+                minopen--;
+                maxopen++;
+            }
+
+            if(maxopen < 0){
+
+                return false;
+            }
+
+            minopen = Math.max(minopen, 0);
+        }
+
+        return minopen == 0;
+        
+    }
+}
