bb9aedc9e77018fe770eb8f9ffb73205f6785d95
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-06-27 21:28:49 +0530

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int current = 0;
+        int max = nums[0];
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(current < 0){
+
+                current = 0;
+            }
+
+            current = current + nums[i];
+            max = Math.max(max, current);
+        }
+
+        return max;
+        
+    }
+}
