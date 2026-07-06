18f88cdf409194a057ccd8f5447cb5b153b34494
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-07-06 20:18:05 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        //edge case
+
+        if(nums.length < 2) return nums[0];
+
+        int[] skiplast = new int[nums.length];
+        int[] skipfirst = new int[nums.length];
+
+        for(int i = 0; i < nums.length - 1; i++){
+
+            skiplast[i] = nums[i];
+            skipfirst[i] = nums[i + 1];
+        }
+
+        int lootskiplast = robHelper(skiplast);
+        int lootskipfirst = robHelper(skipfirst);
+
+        return Math.max(lootskiplast, lootskipfirst);
+        
+    }
+
+    private int robHelper(int[] nums){
+
+        //edge case
+
+        if(nums.length < 2) return nums[0];
+
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.length; i++){
+
+            dp[i] = Math.max((dp[i - 2] + nums[i]), (dp[i - 1]));
+        }
+
+        return dp[nums.length - 1];
+    }
+}
