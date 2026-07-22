52c6a442ccf260616f492df95015f64fbf9e6958
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-07-22 19:49:14 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int value = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            value = value ^ nums[i];
+        }
+
+        return value;
+        
+    }
+}
