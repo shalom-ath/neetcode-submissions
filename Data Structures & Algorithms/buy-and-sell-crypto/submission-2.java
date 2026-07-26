950c14c759cdb377711a0caca6f78dfe17971dce
K Shalom Arpitha <22311A04G1@ece.sreenidhi.edu.in>
2026-07-27 03:52:31 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int max_profit = 0;
+        int buy_price = prices[0];
+
+        for(int i = 1; i < prices.length; i++){
+
+            int current_profit = prices[i] - buy_price;
+
+            if(current_profit > max_profit){
+
+                max_profit = current_profit;
+            }
+
+            if(prices[i] < buy_price){
+
+                buy_price = prices[i];
+            }
+        }
+
+        return max_profit;
+        
+    }
+}
